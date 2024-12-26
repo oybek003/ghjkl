41657418464ac8aa259e887236abb8aceb94e31c
oybek003 <ojbekmuslimov1@gmail.com>
2024-12-26 15:41:48 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let fruits = ['olma', 'banan', 'gilos', 'shoftoli'];
+console.log(fruits);
+
+alert("Boshlanishida arrayingiz:4");
+
+alert("sizning arrayingizdan malumotlarni olib tashlamoqchimiz");
+
+alert("arrayingiz uzunligi 3");
+
+fruits.pop();
+console.log(fruits);
